87a068ea3f496df82889239dfd552f031b7ea436
Lucas Pedroso <lucas.scpedroso@gmail.com>
2023-05-14 14:20:50 +0100

    installed react-router-dom and created routes

--- src/pages/New/index.jsx
+++ src/pages/New/index.jsx
@@ -4,6 +4,7 @@ import { Input } from '../../components/Input'
 import { Textarea } from '../../components/Textarea'
 import { NoteItem } from '../../components/NoteItem'
 import { Section } from '../../components/Section'
+import { Button } from '../../components/Button'
 
 export function New() {
     return (
@@ -24,6 +25,16 @@ export function New() {
                         <NoteItem value="https://example.com"/>
                         <NoteItem isNew placeholder="New link"/>
                     </Section>
+
+                    <Section title="Tags">
+                        <div className='tags'>
+                            <NoteItem value="React"/>
+                            <NoteItem isNew placeholder="New tag"/>
+                        </div>
+                    </Section>
+
+                    <Button title="Save"/>
+
                 </Form>
             </main>
         </Container>
