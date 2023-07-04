975fee730735997c45a6ec371ff8316b6e2ced25
Lucas Pedroso <lucas.scpedroso@gmail.com>
2023-07-04 19:10:24 +0100

    installed axios and created function to comunicate with the backend

--- src/pages/SignUp/index.jsx
+++ src/pages/SignUp/index.jsx
@@ -1,11 +1,40 @@
+import { useState } from "react"
 import { Container, Form, Background } from './styles'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
+
+import { api } from "../../services/api"
 
 import { Input } from '../../components/Input'
 import { Button } from '../../components/Button'
 import { FiMail, FiLock, FiUser } from 'react-icons/fi'
 
 export function SignUp() {
+    const [name, setName] = useState("")
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+
+    const navigate = useNavigate()
+
+    function handleSignUp() {
+        if(!name || !email || !password) {
+            return alert("Please enter all required fields")
+        }
+
+        api.post("/users", { name, email, password})
+        .then(() => {
+            alert("User successfully created")
+            navigate("/")
+        })
+        .catch(error => {
+            if(error.response) {
+                alert(error.response.data.message)
+            } else {
+                alert("Unfortunately, the user could not be created.")
+            }
+        })
+    }
+
+
     return (
         <Container>
             <Background />
@@ -20,20 +49,23 @@ export function SignUp() {
                     placeholder="Enter your name"
                     type="text"
                     icon={FiUser}
+                    onChange={e => setName(e.target.value)}
                 />
 
                 <Input 
                     placeholder="E-mail"
                     type="text"
                     icon={FiMail}
+                    onChange={e => setEmail(e.target.value)}
                 />
                 <Input 
                     placeholder="password"
                     type="password"
                     icon={FiLock}
+                    onChange={e => setPassword(e.target.value)}
                 />
                 
-                <Button title="Sign Up"/>
+                <Button title="Sign Up" onClick={handleSignUp}/>
 
                 <Link to="/">
                     Log In
